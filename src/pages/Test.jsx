3dda69e7603e0b11722cbb6dcac1933660cb534b
MarjorieThomassin <marjorie.thomassin5@gmail.com>
2021-12-27 00:35:23 +0100

    created home page and design

---
+++ src/pages/Test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/test.css";
+
+function Test() {
+  return (
+    <div className="container">
+      <h1>
+        <span id="animation">reveal</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Test;
